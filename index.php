562505e199f085501d60c11eb93fdc89caad987d
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-05 04:17:46 +0100

    add php data

--- index.php
+++ index.php
@@ -1,6 +1,17 @@
 <?php
 
+    // ARRAY CARATTERI
+    $characters=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
+                '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ':', ';', '"', "'",'<', '>', ',', '.', '?', '/'
+    ];
 
+
+    // PASSWORD LENGTH DAL FORM
+    $password_len = !empty($_GET['pass-length']);
+
+    
 
 
 ?>
@@ -38,7 +49,7 @@
             <div class="mb-3 d-flex">
 
                 <label class="form-label">Numero caratteri</label>
-                <input type="number" class="form-control text-center">
+                <input type="number" placeholder="min 5 - max 90" min="5" max="90" class="form-control text-center" name="pass-length">
                 <button type="button" class="btn btn-primary">Send</button>
 
             </div>
