15dd5b6c7f89262ca503532144380a39f1967879
nabilmoutahir <nabil.moutahir97@gmail.com>
2024-03-05 14:17:29 +0100

    fix data

--- index.php
+++ index.php
@@ -1,28 +1,19 @@
 <?php
 
+    // LUNGHEZZA PASSWORD
+    $password_len = !empty($_GET["pass-len"]) ? $_GET['pass-len'] : 5;
+    
+    var_dump($password_len);
+    
     // ARRAY CARATTERI
-    $characters=['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
-                0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
-                '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ':', ';', '"', "'",'<', '>', ',', '.', '?', '/'
+    $characters=[
+        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+        'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+        '!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '+', '=', '{', '}', '[', ']', '|', ':', ';', '"', "'",'<', '>', ',', '.', '?', '/'
+        ,0, 1, 2, 3, 4, 5, 6, 7, 8, 9,
     ];
 
-
-    // PASSWORD LENGTH DAL FORM
-    $password_len = !empty($_GET['pass-length']);
-
-    
-
-
 ?>
-
-
-
-
-
-
-
-
 
 
 
@@ -42,29 +33,28 @@
 
     <div class="d-flex justify-content-center align-content-center">
 
-        <form method="get" action="" class="row col-6 mt-5">
+        <form method="GET" action="" class="row col-6 mt-5">
 
-            <h1 class="text-center">Password Generator</h1>
+            
 
-            <div class="mb-3 d-flex">
-
-                <label class="form-label">Numero caratteri</label>
-                <input type="number" placeholder="min 5 - max 90" min="5" max="90" class="form-control text-center" name="pass-length">
-                <button type="button" class="btn btn-primary">Send</button>
-
-            </div>
-
-
+            <label class="form-label">Numero caratteri</label>
+            
+            <input type="number" name="pass-len" placeholder="min 5 - max 90" min="5" max="90" class="form-control text-center">
+            
+            <button class="btn btn-primary">Send</button>
         </form>
 
-
+        <div>
+            <!-- <?php echo $new ?>  -->
+        </div>
     </div>
 
 
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
-        crossorigin="anonymous"></script>
+        crossorigin="anonymous">
+    </script>
 
 </body>
 
